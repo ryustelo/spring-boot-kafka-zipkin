0039dfc6e5d58a36a95135bc9e77fd0908ec31f1
ryustelo <raul.yuste@tutanota.com>
2023-10-02 22:22:22 +0200

    Added traceability

--- src/main/java/com/transactions/challenge/ingestion/service/TransactionsService.java
+++ src/main/java/com/transactions/challenge/ingestion/service/TransactionsService.java
@@ -3,6 +3,7 @@ package com.transactions.challenge.ingestion.service;
 import com.transactions.challenge.ingestion.api.model.TransactionRequest;
 import com.transactions.challenge.ingestion.exception.TransactionPublisherException;
 import com.transactions.challenge.ingestion.repository.TransactionsPublisher;
+import io.micrometer.observation.annotation.Observed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,7 @@ public class TransactionsService {
         this.transactionsPublisher = transactionsPublisher;
     }
 
+    @Observed(contextualName = "ingest-transaction")
     public void ingest(TransactionRequest transactionRequest) {
         log.info("Transaction requested to ingest: {}", transactionRequest);
 
