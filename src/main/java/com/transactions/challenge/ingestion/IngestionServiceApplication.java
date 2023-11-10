4d031953940d41410728e42f9f19fd032cbc8dcd
ryustelo <raul.yuste@tutanota.com>
2023-11-10 19:18:05 +0100

    Zipkin integration

--- src/main/java/com/transactions/challenge/ingestion/IngestionServiceApplication.java
+++ src/main/java/com/transactions/challenge/ingestion/IngestionServiceApplication.java
@@ -2,13 +2,11 @@ package com.transactions.challenge.ingestion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
-@EnableKafka
 @SpringBootApplication
 public class IngestionServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IngestionServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IngestionServiceApplication.class, args);
+    }
 }
--- src/main/java/com/transactions/challenge/ingestion/configuration/KafkaConfiguration.java
+++ src/main/java/com/transactions/challenge/ingestion/configuration/KafkaConfiguration.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
+@EnableKafka
 public class KafkaConfiguration {
 
     private final KafkaProperties properties;
